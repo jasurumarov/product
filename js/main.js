c916537abc47134bea05080d7172eb798646b17a
jasurumarov <jasurumarov199@gmail.com>
2024-01-10 18:52:22 +0500

    backtop added

--- js/main.js
+++ js/main.js
@@ -21,3 +21,22 @@ let modeBtn = document.getElementById("light-dark");
 modeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 }); 
+
+// BACKTOP ////////////////////
+  
+window.addEventListener('scroll', function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById('backtop');
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = '20px';
+  } else {
+    backtop.style.bottom = '-60px';
+  }
+}
