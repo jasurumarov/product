b6343b3dfd7111698fda058debeac91597fdcc80
jasurumarov <jasurumarov199@gmail.com>
2024-01-11 12:12:05 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -48,3 +48,19 @@ const menuBtn = document.querySelector(".menu-btn");
 menuBtn.addEventListener("click", function () {
     document.body.classList.toggle("menu-opened")
 });
+
+// NAVBAR SHRINK /////////////////////////////
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
